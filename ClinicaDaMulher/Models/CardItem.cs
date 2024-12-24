d50043b655319868d6cf715554a7ae176da6e893
David Freitas <davidfreitas51@proton.me>
2024-12-24 18:36:39 -0300

    Finished landing page and started login

---
+++ ClinicaDaMulher/Models/CardItem.cs
@@ -0,0 +1,9 @@
+﻿namespace ClinicaDaMulher.Models;
+
+public class CardItem
+{
+    public string Image { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string AdditionalInfo { get; set; }
+}
